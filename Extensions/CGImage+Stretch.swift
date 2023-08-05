cfef04a5d890e9ca438664531418a606270af6da
James Wilson <jw@jameswilson.io>
2023-08-05 12:46:19 +1000

    Stretch refactor

--- Extensions/CGImage+Stretch.swift
+++ Extensions/CGImage+Stretch.swift
@@ -8,14 +8,19 @@
 import CoreGraphics
 import Foundation
 
+struct StretchParameters {
+    var shadowClip: Float
+    var midtone: Float
+}
+
 extension CGImage {
-    var unsortedPixels: [Float]? {
-        var pixels = [Float](repeating: 0, count: height * width)
+    func unsortedPixels(ofWidth width: Int, height: Int) -> [Float]? {
+        var pixels = [Float](repeating: 0, count: width * height)
         guard let context = CGContext(data: &pixels,
                                       width: width,
                                       height: height,
-                                      bitsPerComponent: bitsPerComponent,
-                                      bytesPerRow: bytesPerRow,
+                                      bitsPerComponent: MemoryLayout<Float>.size * 8,
+                                      bytesPerRow: MemoryLayout<Float>.size * width,
                                       space: colorSpace ?? CGColorSpaceCreateDeviceGray(),
                                       bitmapInfo: bitmapInfo.rawValue) else { return nil }
         context.draw(self, in: CGRect(origin: .zero, size: CGSize(width: width, height: height)))
@@ -35,58 +40,67 @@ extension CGImage {
         }
     }
     
-    var stretchedImage: CGImage? {
-        print("START")
-        guard let pixels = self.unsortedPixels else { return nil }
-        print("LOADED UNSORTED")
-        print("SORTING...")
-        var sortedFP = pixels.sorted { x, y in
-            x > y
-        }
+    var stretchParameters: StretchParameters? {
+        print("GETTING DOWN-SIZED PIXELS")
+        let subframeWidth = width / 4
+        let subframeHeight = height / 4
+        guard var pixels = self.unsortedPixels(ofWidth: subframeWidth, height: subframeHeight)?.sorted()
+        else { return nil }
         print("SORTED")
-        let max = sortedFP[0]
+        guard let max = pixels.last else { return nil }
         print("MAX: ", max)
-            
-        for i in 0 ..< sortedFP.count {
-            sortedFP[i] = sortedFP[i] / max
+        
+        for i in 0 ..< pixels.count {
+            pixels[i] = pixels[i] / max
         }
-            
+        
         var median: Float = 0.0
-        if sortedFP.count % 2 == 0 {
-            median = (sortedFP[sortedFP.count / 2] + sortedFP[(sortedFP.count / 2) - 1]) / 2.0
+        if pixels.count % 2 == 0 {
+            median = (pixels[pixels.count / 2] + pixels[(pixels.count / 2) - 1]) / 2.0
         } else {
-            median = sortedFP[(sortedFP.count - 1) / 2]
+            median = pixels[(pixels.count - 1) / 2]
         }
         print("CALCULATED MEDIAN: ", median)
-            
-        var deviations = [Float](repeating: 0.0, count: sortedFP.count)
-        for i in 0 ..< sortedFP.count {
-            deviations[i] = abs(sortedFP[i] - median)
+        
+        var deviations = [Float](repeating: 0.0, count: pixels.count)
+        for i in 0 ..< pixels.count {
+            deviations[i] = abs(pixels[i] - median)
         }
         let avgMedDev = deviations.reduce(0, +) / Float(deviations.count)
         print("AVG MEDIAN DEVIATION: ", avgMedDev)
-            
+        
         let shadowClipConst = Float(-1.25)
         let shadowClip = median + (shadowClipConst * avgMedDev)
         print("SHADOW CLIP: ", shadowClip)
         let targetBG = Float(0.25)
         let midtone = self.mtf(midtone: targetBG, x: median - shadowClip)
         print("MIDTONE: ", midtone)
-        var out = [Float](repeating: 0.0, count: sortedFP.count)
+
+        return StretchParameters(shadowClip: shadowClip, midtone: midtone)
+    }
+    
+    var stretchedImage: CGImage? {
+        print("START")
+        print("LOADED UNSORTED")
+        print("SORTING...")
+        guard let params = stretchParameters else { return nil }
+
+        guard let pixels = self.unsortedPixels(ofWidth: width, height: height) else { return nil }
+        var out = [Float](repeating: 0.0, count: pixels.count)
         for i in 0 ..< pixels.count {
-            if pixels[i] < shadowClip {
+            if pixels[i] < params.shadowClip {
                 out[i] = 0
             } else {
-                let normalised = (pixels[i] - shadowClip) / (1 - shadowClip)
+                let normalised = (pixels[i] - params.shadowClip) / (1 - params.shadowClip)
                 switch normalised {
                 case 0:
                     out[i] = 0
-                case midtone:
+                case params.midtone:
                     out[i] = 0.5
                 case 1:
                     out[i] = 1
                 default:
-                    out[i] = (midtone - 1) * normalised / ((((2 * midtone) - 1) * normalised) - midtone)
+                    out[i] = (params.midtone - 1) * normalised / ((((2 * params.midtone) - 1) * normalised) - params.midtone)
                 }
             }
         }
@@ -96,8 +110,8 @@ extension CGImage {
         let image = CGImage(
             width: self.width,
             height: self.height,
-            bitsPerComponent: 32,
-            bitsPerPixel: 32,
+            bitsPerComponent: MemoryLayout<Float>.size * 8,
+            bitsPerPixel: MemoryLayout<Float>.size * 8,
             bytesPerRow: self.width * 4,
             space: CGColorSpaceCreateDeviceGray(),
             bitmapInfo: CGBitmapInfo(rawValue:
---
+++ Views/Toolbar/FileListModePicker.swift
@@ -0,0 +1,18 @@
+//
+//  FileListModePicker.swift
+//  Astro
+//
+//  Created by James Wilson on 5/8/2023.
+//
+
+import SwiftUI
+
+struct FileListModePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FileListModePicker()
+}
