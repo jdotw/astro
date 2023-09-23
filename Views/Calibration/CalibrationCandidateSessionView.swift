c2b509ca3c4b9494011daa7f50646bce522a419b
James Wilson <jw@jameswilson.io>
2023-09-23 11:25:34 +1000

    dynamically update session info on drop

--- Views/Calibration/CalibrationCandidateSessionView.swift
+++ Views/Calibration/CalibrationCandidateSessionView.swift
@@ -9,18 +9,24 @@ import SwiftUI
 
 struct CalibrationCandidateSessionView: View {
     @ObservedObject var session: Session
+    @FetchRequest var files: FetchedResults<File>
+
+    init(session: Session) {
+        self.session = session
+        _files = FetchRequest(
+            sortDescriptors: [],
+            predicate: NSPredicate(format: "session = %@", session),
+            animation: .default)
+    }
 
     var body: some View {
         VStack(alignment: .leading) {
             Text(session.dateString)
-            Text(session.uniqueUncalibratedFilterNames.joined(separator: ", "))
+            Text(uniqueUncalibratedFilterNames.joined(separator: ", "))
         }.draggable(session.objectID.uriRepresentation())
     }
-}
 
-extension Session {
     var uniqueUncalibratedFilterNames: [String] {
-        guard let files = files?.allObjects as? [File] else { return [] }
         let filters = files.compactMap { $0.calibrationSession != nil ? nil : $0.filter }
         return Array(Set(filters)).sorted()
     }
